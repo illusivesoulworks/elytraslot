4c10c1df74e1228a6133581d42bc46ed3c67443b
TheIllusiveC4 <itwasmec4@gmail.com>
2020-07-25 20:52:24 -0700

    update copyright and README.md

--- docs/CHANGELOG.md
+++ docs/CHANGELOG.md
@@ -1,9 +1,7 @@
 # Changelog
 All notable changes to this project will be documented in this file.
 
-The format is based on [Keep a Changelog](http://keepachangelog.com/en/1.0.0/) and this project does not adhere to [Semantic Versioning](http://semver.org/spec/v2.0.0.html).
-This project uses MCVERSION-MAJORMOD.MAJORAPI.MINOR.PATCH.
+The format is based on [Keep a Changelog](http://keepachangelog.com/en/1.0.0/) and this project adheres to [Semantic Versioning](http://semver.org/spec/v2.0.0.html).
 
-## [3.0](https://github.com/TheIllusiveC4/CuriousElytra/compare/1.15.x...master) - 2020.07.02
-### Changed
-- Ported to 1.16.1 Forge
+## [1.0.0] - 2020.07.25
+Initial release
--- src/main/java/top/theillusivec4/curiouselytra/loader/impl/AccessorImpl.java
+++ src/main/java/top/theillusivec4/curiouselytra/loader/impl/AccessorImpl.java
@@ -1,3 +1,22 @@
+/*
+ * Copyright (c) 2019-2020 C4
+ *
+ * This file is part of Curious Elytra, a mod made for Minecraft.
+ *
+ * Curious Elytra is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Curious Elytra is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with Curious Elytra.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
 package top.theillusivec4.curiouselytra.loader.impl;
 
 import net.minecraft.entity.LivingEntity;
