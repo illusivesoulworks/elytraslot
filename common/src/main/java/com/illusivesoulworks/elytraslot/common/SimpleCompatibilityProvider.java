5b6d5358c067c7238943a662ac5d152edc03e3ca
mango_buff <208079635@qq.com>
2025-08-15 19:25:44 +0800

    Fixed an issue preventing Netherite Elytra from rendering properly.

--- common/src/main/java/com/illusivesoulworks/elytraslot/common/SimpleCompatibilityProvider.java
+++ common/src/main/java/com/illusivesoulworks/elytraslot/common/SimpleCompatibilityProvider.java
@@ -104,7 +104,7 @@ public class SimpleCompatibilityProvider implements IElytraProvider {
 
       if (isLoaded.test("netherite_ext")) {
         ID_TO_TEXTURE.put("netherite_ext:netherite_elytra",
-                new ResourceLocation("netherite_ext:textures/entity/netherite_elytra.png"));
+            new ResourceLocation("netherite_ext:textures/entity/netherite_elytra.png"));
       }
       init = true;
     }
